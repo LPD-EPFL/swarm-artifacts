38f08fe04ab0a0a29ee183f0df680039b1e7bede
Clément Burgelin <zyfarok@gmail.com>
2024-08-21 23:31:02 +0200

    improve print-datapoints/fig5...

--- print-datapoints/fig5-latency-cdf.py
+++ print-datapoints/fig5-latency-cdf.py
@@ -3,30 +3,22 @@
 import os
 from logparser import parse
 
-schemes = {
-    'RAW',
-    'SWARM-KV',
-    'DM-ABD',
-    'FUSEE',
-}
+ptiles = [0.1] + list(range(1,100)) + [99.9]
 
-ops = [
-    'GET',
-    'UPDATE',
-]
-
-for workload in ["workload-B", "workload-A"]:
+for workload in ["workload-A", "workload-B"]:
     print(f'{workload}:')
 
-    ptiles = [0.1] + list(range(1,100)) + [99.9]
+    for op in ['GET', 'UPDATE']:
+        print(f'- {op}s:')
 
-    for op in ops:
-            print(f'  - Latency percentiles of {op}s:')
-            for i, s in enumerate(schemes):
-                    print(f'    - for {s}:')
-                    path = os.path.join('logs',
-                        f'fig5-latency-cdf/{workload}/{s}/client1.txt',
-                    )
-                    data = parse(path)[op]
-                    for ptile in ptiles:
-                        print(f'      - {ptile}%: {data[ptile]}μs')
+        for s in ['RAW', 'SWARM-KV', 'DM-ABD', 'FUSEE']:
+            print(f'  - Latency percentiles of {op}s for {s}:')
+
+            path = os.path.join('logs',
+                f'fig5-latency-cdf/{workload}/{s}/client1.txt',
+            )
+            data = parse(path)[op]
+
+            for ptile in ptiles:
+                print(f'    - {ptile}%: {data[ptile]}μs')
+print("Note that the graph only shows the experiment on workload-B")
