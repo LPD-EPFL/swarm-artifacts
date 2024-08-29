129e20dd6ca6cf35655877742db4e7720cb68df8
Clément Burgelin <zyfarok@gmail.com>
2024-08-29 17:02:00 +0200

    use rm -rf

--- send-deployment.sh
+++ send-deployment.sh
@@ -9,5 +9,5 @@ for i in {1..8}; do
   echo "Sending deployment to w$i"
   ssh w$i "mkdir -p \"$BASE_DIR\""
   scp deployment.zip w$i:"$BASE_DIR/deployment.zip"
-  ssh w$i "unzip -o \"$BASE_DIR/deployment.zip\" -d \"$BASE_DIR\"; cd \"$BASE_DIR\"; tar -xf ycsb-0.12.0.tar.gz; rm -r YCSB/ 2> /dev/null;  mv ycsb-0.12.0 YCSB; cd bin; unzip -o bin.zip;"
+  ssh w$i "unzip -o \"$BASE_DIR/deployment.zip\" -d \"$BASE_DIR\"; cd \"$BASE_DIR\"; tar -xf ycsb-0.12.0.tar.gz; rm -rf YCSB; mv ycsb-0.12.0 YCSB; cd bin; unzip -o bin.zip;"
 done
--- undo-deployment.sh
+++ undo-deployment.sh
@@ -4,5 +4,5 @@ set -e
 BASE_DIR="$( realpath -sm  "$( dirname "${BASH_SOURCE[0]}" )")"
 
 for i in {1..8}; do
-  ssh w$i "rm -r \"$BASE_DIR\"" | true
+  ssh w$i "rm -rf \"$BASE_DIR\""
 done
