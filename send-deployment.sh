3c969705184f8581b0757d5d6e08aa06a7bfaece
Clément Burgelin <zyfarok@gmail.com>
2024-08-29 11:17:45 +0200

    Making sure YCSB deployment goes well

--- send-deployment.sh
+++ send-deployment.sh
@@ -10,5 +10,5 @@ for i in {1..8}; do
   echo "Sending deployment to w$i"
   ssh w$i "mkdir -p \"$PARENT_DIR\""
   scp deployment.zip w$i:"$PARENT_DIR/deployment.zip"
-  ssh w$i "unzip -o \"$PARENT_DIR/deployment.zip\" -d \"$BASE_DIR\"; cd \"$BASE_DIR\"; tar -xf ycsb-0.12.0.tar.gz; mv ycsb-0.12.0 YCSB; cd bin; unzip -o bin.zip;"
+  ssh w$i "unzip -o \"$PARENT_DIR/deployment.zip\" -d \"$BASE_DIR\"; cd \"$BASE_DIR\"; tar -xf ycsb-0.12.0.tar.gz; rm -r YCSB/ 2> /dev/null;  mv ycsb-0.12.0 YCSB; cd bin; unzip -o bin.zip;"
 done
