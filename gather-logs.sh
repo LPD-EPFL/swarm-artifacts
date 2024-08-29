868210770ee4e5b3f2bd83ca996561b8c4e37028
Clément Burgelin <zyfarok@gmail.com>
2024-08-29 17:02:12 +0200

    cleanup after gathering logs

--- gather-logs.sh
+++ gather-logs.sh
@@ -9,4 +9,5 @@ for i in {1..8}; do
   ssh w$i "cd \"$BASE_DIR\"; rm -rf logs.zip; zip -r logs.zip logs/"
   scp w$i:"$BASE_DIR/logs.zip" w$i-logs.zip
   unzip -o w$i-logs.zip
+  rm -rf w?-logs.zip
 done
