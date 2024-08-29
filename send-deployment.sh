a38bfcda62b22693b63e1fc3bc85ac7dbc21dde1
Clément Burgelin <zyfarok@gmail.com>
2024-08-29 16:27:07 +0200

    Simplify deployment script to match readme

--- send-deployment.sh
+++ send-deployment.sh
@@ -2,13 +2,12 @@
 set -e
 
 BASE_DIR="$( realpath -sm  "$( dirname "${BASH_SOURCE[0]}" )")"
-PARENT_DIR="$( realpath -sm  "${BASE_DIR}/..")"
 
 cd "$BASE_DIR"
 
 for i in {1..8}; do
   echo "Sending deployment to w$i"
-  ssh w$i "mkdir -p \"$PARENT_DIR\""
-  scp deployment.zip w$i:"$PARENT_DIR/deployment.zip"
-  ssh w$i "unzip -o \"$PARENT_DIR/deployment.zip\" -d \"$BASE_DIR\"; cd \"$BASE_DIR\"; tar -xf ycsb-0.12.0.tar.gz; rm -r YCSB/ 2> /dev/null;  mv ycsb-0.12.0 YCSB; cd bin; unzip -o bin.zip;"
+  ssh w$i "mkdir -p \"$BASE_DIR\""
+  scp deployment.zip w$i:"$BASE_DIR/deployment.zip"
+  ssh w$i "unzip -o \"$BASE_DIR/deployment.zip\" -d \"$BASE_DIR\"; cd \"$BASE_DIR\"; tar -xf ycsb-0.12.0.tar.gz; rm -r YCSB/ 2> /dev/null;  mv ycsb-0.12.0 YCSB; cd bin; unzip -o bin.zip;"
 done
--- undo-deployment.sh
+++ undo-deployment.sh
@@ -2,10 +2,7 @@
 set -e
 
 BASE_DIR="$( realpath -sm  "$( dirname "${BASH_SOURCE[0]}" )")"
-PARENT_DIR="$( realpath -sm  "${BASE_DIR}/..")"
-
-cd "$BASE_DIR"
 
 for i in {1..8}; do
-  ssh w$i "rm -r \"$PARENT_DIR/deployment.zip\" \"$BASE_DIR\"" | true
+  ssh w$i "rm -r \"$BASE_DIR\"" | true
 done
